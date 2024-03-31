4ceed35eb5ed647c82a2f81862e850f81ed73ceb
shubham <shubhamsagar628@gmail.com>
2024-03-31 11:21:11 +0530

    Implementing MAil Sending Feature in OTP Models

--- Models/otp.js
+++ Models/otp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const mailSender = require('../Utilis/mailSender')
 
 const otpSchema = new mongoose.Schema({
     email:{
@@ -14,6 +15,22 @@ const otpSchema = new mongoose.Schema({
         default:Date.now(),
         expires:5*60
     }
+})
+
+const sendVerificationOnEmail = async(email,otp) => {
+    try{
+        const mailResponse = await mailSender(email,"Verification Email from StudyNotion",otp)
+        console.log("Email sent Successfully: " , mailResponse)
+    }catch(error){
+        console.log("Error occured while sending mails: ", error)
+        throw error
+    }
+}
+
+
+otpSchema.pre('save',async function(next) {
+    await sendVerificationOnEmail(this.email,this.otp)
+    next()
 })
 
 module.exports = mongoose.model("OTP",otpSchema)
